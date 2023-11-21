02a764c41fc8a7b5a2d3af9872a7e1b4e547cec9
mcventur <marcelinopenide@gmail.com>
2023-11-21 22:51:05 +0100

    Proyecto de partida 4

--- src/Panadero.java
+++ src/Panadero.java
@@ -1,5 +1,11 @@
 //TODO El panadero instanciará una barra de pan y la dejará en el mostrador
 public class Panadero extends Thread{
+    private Panaderia panaderia;
+
+    public Panadero(Panaderia panaderia) {
+        this.panaderia = panaderia;
+    }
+
     @Override
     public void run() {
 
--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,5 +1,11 @@
 //TODO los vendedores venden barras de pan mientras la panaderia esté abierta
 public class Vendedor extends Thread{
+    private Panaderia panaderia;
+
+    public Vendedor(Panaderia panaderia) {
+        this.panaderia = panaderia;
+    }
+
     @Override
     public void run() {
 
